e347e0cad70be3e7c57c959765b00b105943114e
flycash <mingflycash@gmail.com>
2020-10-27 22:02:59 +0800

    httplib filter example

---
+++ httplib/filter/prometheus/main.go
@@ -0,0 +1,39 @@
+// Copyright 2020 
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package main
+
+import (
+	"github.com/astaxie/beego/client/httplib"
+	"github.com/astaxie/beego/client/httplib/filter/prometheus"
+	"github.com/astaxie/beego/core/logs"
+)
+
+func main() {
+	builder := prometheus.FilterChainBuilder{
+		AppName: "My-test",
+		ServerName: "User-server-1",
+		RunMode: "dev",
+	}
+	req := httplib.Get("http://beego.me/")
+	// only work for this request, or using SetDefaultSetting to support all requests
+	req.AddFilters(builder.FilterChain)
+
+	resp, err := req.Response()
+	if err != nil {
+		logs.Error("could not get response: ", err)
+	} else {
+		logs.Info(resp)
+	}
+}
